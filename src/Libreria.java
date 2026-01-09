98d56d8519d2e64e9f88268d7fd80b209728e259
caarmeencv <carmencv253@gmail.com>
2026-01-09 12:51:38 +0100

    09/01

--- src/Libreria.java
+++ src/Libreria.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import service.AutorService;
 import service.EditorialService;
@@ -5,49 +6,54 @@ import service.LibroService;
 
 public class Libreria {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
 
-        AutorService autorService = new AutorService();
-        LibroService libroService = new LibroService();
+        int opcion = 0;
+
         EditorialService editorialService = new EditorialService();
+        LibroService libroService = new LibroService();
+        AutorService autorService = new AutorService();
 
-        int opcion;
 
-        do {
-            System.out.println("\n==============================");
-            System.out.println("        GESTIÓN LIBRERÍA      ");
-            System.out.println("==============================");
-            System.out.println("1. Editoriales");
-            System.out.println("2. Libros");
-            System.out.println("3. Autores");
-            System.out.println("0. Salir");
-            System.out.print("Elige una opción: ");
+        while(opcion != 4){
+            mostrarMenuPrincipal();
+            try {
+                opcion = sc.nextInt();
+                sc.nextLine();
 
-            opcion = sc.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    editorialService.menu();
-                    break;
-                case 2:
-                    libroService.menu();
-                    break;
-                case 3:
-                    autorService.menu();
-                    break;
-                case 0:
-                    System.out.println("Saliendo del programa...");
-                    break;
-                default:
-                    System.out.println("Opción no válida.");
-                    break;
+                switch (opcion) {
+                    case 1:
+                        editorialService.menu();
+                        break;
+                    case 2:
+                        libroService.menu();
+                        break;
+                    case 3:
+                        autorService.menu();
+                        break;
+                    case 4:
+                        System.out.println("Saliendo del programa...");
+                        break;
+                    default:
+                        System.out.println("Opción no válida. Por favor, elige una opción del 1 al 4.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada no válida. Por favor, introduce un número.");
+                sc.nextLine();
             }
-
-        } while (opcion != 0);
-
+        }
         sc.close();
         System.out.println("Programa finalizado.");
+    }
+
+    private static void mostrarMenuPrincipal() {
+        System.out.println("\nMENÚ PRINCIPAL");
+            System.out.println("1. Menú de editoriales");
+            System.out.println("2. Menú de libros");
+            System.out.println("3. Menú de autores");
+            System.out.println("4. Salir");
+            System.out.print("Elige una opción: ");
     }
 }
